3254ee3b5c25ded5492f1ca587f361385b04b80c
hook1700 <409541523@qq.com>
2021-11-01 19:04:13 +0800

    project create

---
+++ dao/redis/redis.go
@@ -0,0 +1,27 @@
+package redis
+
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+)
+
+// 声明一个全局的rdb变量
+var rdb *redis.Client
+
+// 初始化连接
+
+func InitClient() (err error) {
+	rdb = redis.NewClient(&redis.Options{
+		Addr:     "127.0.0.1:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err = rdb.Ping().Result()
+	if err != nil {
+		return err
+	}
+	//连接成功提示
+	fmt.Println("redis连接成功")
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"redisData/dao/redis"
+)
+
+func main() {
+	//初始化redis
+	err := redis.InitClient()
+	if err != nil {
+		fmt.Printf("init redis fail err:%v/n", err)
+		return
+	}
+
+	r := gin.Default()
+	r.GET("/hello", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "hello",
+		})
+	})
+
+	r.GET("/test", func(c *gin.Context) {
+		response, err := http.Get("https://baidu.com")
+		if err != nil || response.StatusCode != http.StatusOK {
+			c.Status(http.StatusServiceUnavailable)
+			return
+		}
+
+		reader := response.Body
+		contentLength := response.ContentLength
+		contentType := response.Header.Get("Content-Type")
+
+		extraHeaders := map[string]string{
+			//"Content-Disposition": `attachment; filename="gopher.png"`,
+		}
+
+		c.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
+	})
+
+	//请求的 url https://api.huobi.pro/market/history/kline?period=1min&size=1&symbol=btcusdt
+	r.GET("/redisData/:period/:size/:symbol", func(c *gin.Context) {
+		//校验参数，不写直接给默认值
+
+		//逻辑处理 每30s发起请求 起10多个goroutine同时发起请求 通过数据拼接发起请求 拿到响应数据 存入redis同时发送到服务器
+
+		//返回数据
+	})
+	//查询，查询redis上的数据，返回给前端
+	r.GET("/getRedisData/:period/:size/:symbol", func(c *gin.Context) {
+		//校验参数，不写直接给默认值
+
+		//逻辑处理 每30s发起请求 起10多个goroutine同时发起请求 通过数据拼接发起请求 拿到响应数据 存入redis同时发送到服务器
+
+		//返回数据
+	})
+	r.Run(":8887")
+}
